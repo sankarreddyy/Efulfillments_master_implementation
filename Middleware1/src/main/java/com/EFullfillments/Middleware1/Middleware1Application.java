000a66d5f783e1fe24699a2cc4c0d73992e829b3
sankarreddyy <40375085+sankarreddyy@users.noreply.github.com>
2018-07-21 20:04:42 -0500

    Created New project

---
+++ Middleware1/src/main/java/com/EFullfillments/Middleware1/Middleware1Application.java
@@ -0,0 +1,12 @@
+package com.EFullfillments.Middleware1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Middleware1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Middleware1Application.class, args);
+	}
+}
